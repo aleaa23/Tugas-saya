eab91c3b7d0acfe63d70aac70fc4f8e2b82321b5
aleaa23 <belaalya007@gmail.com>
2025-01-15 12:12:10 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,16 @@
+
+let nama = "alea natasa"; 
+let alamat = "karanggeneng"; 
+let usia = "16";  
+let ttl = "demak 23,oktober 2008"; 
+let gender = "perempuan"; 
+let agama = "islam"; 
+let email = "aleaaa23@gmail.com";
+let berat = "42";
+let tinggi = "150";
+let hobi = "menulis ";
+let cita = "bos muda";
+let pekerjaan = "pelajar";
+let sekolah = "SMK N 1 SAYUNG";
+
+document.write("nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + "tahun, saya lahir di " + ttl + "jenis kelamin saya adalah " + gender + "saya beragama " + agama + "alamat email saya adalah " + email + "saya memiliki berat badan " + berat +"kg"+ "dengan tinggi " + tinggi + "saya sangat senang " + hobi + "karena saya memiliki cita cita " + cita + "saya saat adalah seorang " + pekerjaan + "yang bersekolah di " + sekolah + "pada jurusan " + jurusan);
